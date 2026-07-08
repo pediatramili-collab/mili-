7f6bd0e59eb9dd487433d3cdeee70ca0da3744cf
pediatramili-collab <pediatramili@gmail.com>
2026-07-07 22:13:43 -0300

    Delete sgpa-ap4-java/sgpa-ap4 directory

--- sgpa-ap4-java/sgpa-ap4/src/com/bdcine/sgpa/exception/EntidadNoEncontradaException.java
+++
@@ -1,8 +0,0 @@
-package com.bdcine.sgpa.exception;
-
-/** Se lanza cuando no se encuentra una entidad por su ID. */
-public class EntidadNoEncontradaException extends SgpaException {
-    public EntidadNoEncontradaException(String tipo, int id) {
-        super(404, tipo + " con ID " + id + " no encontrado.");
-    }
-}
